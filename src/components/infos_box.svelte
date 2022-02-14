97de4a0cc14de05372031779aa00e7d4c7f476db
salim benmesbah <salimbenmesbah@gmail.com>
2022-02-14 18:53:52 +0100

    m

---
+++ src/components/infos_box.svelte
@@ -0,0 +1,21 @@
+<script>
+
+</script>
+
+<main>
+    <div class="mt-6 max-w-xl mx-auto border-0 rounded-2xl shadow-xl w-3/5">
+        <div class="py-10">
+            <div class="flex justify-center font-black">
+                <p style="color: #707070;" class="text-sm pr-3 font-black">Snowing</p>
+                <p style="color: #0076FF;" class="text-sm pl-3 font-black">12:00 pm</p>
+            </div>
+            <p style="font-size: 50px; font-weight :100;" class="pl-20 pt-2">26°</p>
+            <p class="text-sm pl-20 pt-2 font-black">Columbia Falls, MT</p>
+        </div>
+    </div>
+</main>
+
+<style>
+
+
+</style>
---
+++ src/routes/test/[id].svelte
@@ -0,0 +1,6 @@
+<script>
+   import {  page } from '$app/stores';
+    console.log($page.params.id)
+</script>
+
+test hello {  $page.params.id }
